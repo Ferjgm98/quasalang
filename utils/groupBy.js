ca2b024a5e32d3bcb6e25b95f370f4c6e6abdc06
Fernando Gonzalez <fernandomenjivar2011@gmail.com>
2021-07-20 23:55:57 -0600

    add missing helper grop by

---
+++ utils/groupBy.js
@@ -0,0 +1,24 @@
+const merge = require('deepmerge');
+
+module.exports = function() {
+    this.groupBy = groupBy = (
+        inputArray,
+        key,
+        removeKey = false,
+        outputType = [],
+      ) => {
+        return inputArray.reduce((previous, current) => {
+            // Get the current value that matches the input key and remove the key value for it.
+            const { [key]: keyValue } = current;
+            const formattedKeyObject = doPathToObject(keyValue, current);
+            // remove the key if option is set
+            removeKey && keyValue && delete current[key];
+
+            // Create a new object and return that merges the previous with the current object
+            return merge(previous, formattedKeyObject);
+          },
+          // Replace the object here to an array to change output object to an array
+          outputType,
+        );
+      };
+  }
